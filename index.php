d7942b26e0dfd606721ac707ac2994829a911e7b
GreenSol <dsolomons@salesforce.com>
2017-11-13 16:00:15 +1100

    LPL Code
    First Commit

---
+++ index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<!-- saved from url=(0047)https://v4-alpha.getbootstrap.com/content/code/ -->
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<title>
+
+    PLP-Gen
+
+</title>
+
+
+
+<!-- Meta -->
+<meta name="description" content="Pardot Landing Page Generator. Clicks not code (but some code, but it been made easy, so that's ok...">
+
+
+<!-- Bootstrap core CSS -->
+
+  <style class="anchorjs"></style><link href="./Code · Bootstrap_files/bootstrap.min.css" rel="stylesheet">
+
+
+<!-- Documentation extras -->
+<link href="./Code · Bootstrap_files/docs.min.css" rel="stylesheet">
+
+<!-- Favicons -->
+<link rel="apple-touch-icon" href="https://v4-alpha.getbootstrap.com/apple-touch-icon.png">
+<link rel="icon" href="https://v4-alpha.getbootstrap.com/favicon.ico">
+
+</header>
+
+
+    <div class="bd-pageheader">
+      <div class="container">
+
+  <h1>PLP-Gen</h1>
+  <p class="lead">
+    Pardot Landing Page Generator. Clicks not code (but some code, but it been made easy, so that's ok...
+  </p>
+
+
+
+      </div>
+    </div>
+
+<form action="results.php" method="post">
+              <div class="row">
+                <div class="col-md-3">
+                </div>
+                <div class="col-md-6">
+
+                 <!-- Logo URL -->
+                <div class="form-group">
+                    <input class="form-control" name="var" type="text" placeholder="var" required data-validation-required-message="Please enter your name.">
+                    <p class="help-block text-danger"></p>
+                  </div>
+
+
+<div class="col-lg-12 text-center">
+                  <div id="success"></div>
+                  <button id="sendMessageButton" class="btn btn-primary btn-xl text-uppercase" type="submit">Generate!</button>
+                </div>
+
+   
+
+<script src="./Code · Bootstrap_files/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+<script>window.jQuery || document.write('<script src="/assets/js/vendor/jquery.min.js"><\/script>')</script>
+
+<script src="./Code · Bootstrap_files/tether.min.js"></script>
+
+
+  <script src="./Code · Bootstrap_files/bootstrap.min.js"></script>
+
+
+
+  <script src="./Code · Bootstrap_files/anchor.min.js"></script>
+
+  <script src="./Code · Bootstrap_files/clipboard.min.js"></script>
+
+  <script src="./Code · Bootstrap_files/holder.min.js"></script>
+
+  <script src="./Code · Bootstrap_files/application.js"></script>
+
+
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<script src="./Code · Bootstrap_files/ie10-viewport-bug-workaround.js"></script>
+<script src="./Code · Bootstrap_files/ie-emulation-modes-warning.js"></script>
+
+
+<script src="./Code · Bootstrap_files/jekyll-search.min.js"></script>
+
+
+
+
+<script>
+Holder.addTheme('gray', {
+  bg: '#777',
+  fg: 'rgba(255,255,255,.75)',
+  font: 'Helvetica',
+  fontweight: 'normal'
+})
+</script>
+
+  
+
+</body></html>
