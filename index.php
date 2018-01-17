46ad516ec89ce8d0404a9212b95ab013759fd4a4
GreenSol <dsolomons@salesforce.com>
2018-01-17 16:29:52 +1100

    .

--- index.php
+++ index.php
@@ -23,6 +23,10 @@
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
 
+<!-- Plugin CSS -->
+<link href=“https://blackrockdigital.github.io/startbootstrap-freelancer/vendor/magnific-popup/magnific-popup.css” rel=“stylesheet” type=“text/css”>
+
+
     <!-- Custom styles for this template -->
     <link href="css/freelancer.min.css" rel="stylesheet">
 
