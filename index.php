3dee0e9b89110028d4d3337601574d8c1bce04c3
GreenSol <dsolomons@salesforce.com>
2018-01-17 15:56:29 +1100

    js update

--- index.php
+++ index.php
@@ -414,11 +414,17 @@
     </div>
 
     
+    <script src=“https://code.jquery.com/jquery-3.2.1.slim.min.js” integrity=“sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN” crossorigin=“anonymous”></script>
+    <script src=“https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js” integrity=“sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q” crossorigin=“anonymous”></script>
+    <script src=“https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js” integrity=“sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4” crossorigin=“anonymous”></script>
 
-    <!-- Bootstrap core JavaScript -->
+
+
+    <!-- Bootstrap core JavaScript
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
+ -->
+ 
     <!-- Plugin JavaScript -->
     <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
 
