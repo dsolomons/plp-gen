5c696cfcc2997ae8e64dfe88b03f2d9f1949bb3e
GreenSol <dsolomons@salesforce.com>
2018-05-23 09:40:19 +1000

    .
    .

--- index.php
+++ index.php
@@ -4,14 +4,15 @@
   <head>
 
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-119695125-2"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-119753873-1"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'UA-119695125-2');
+  gtag('config', 'UA-119753873-1');
 </script>
+
 
     
     <!-- Meta -->
